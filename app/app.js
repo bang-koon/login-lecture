8782e922679a8f01f6016dce3a4cf8faeb2c0f22
jomi707 <dol7966@gmail.com>
2022-07-06 16:05:02 +0900

    프론트 js 추가

--- app/app.js
+++ app/app.js
@@ -6,13 +6,16 @@ const app = express();
 
 const PORT = 3000;
 
-
+// routing
+const home = require("./src/routes/home");
 
 // app setting
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-const home = require("./src/routes/home"); // index.js
+// views/login.ejs 에서 js에 접근하게 하는 미들웨어
+app.use(express.static(`${__dirname}/src/public`));
+
 app.use("/", home); // 미들웨어 등록 메서드
 
 module.exports = app;
