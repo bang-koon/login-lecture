89632406f11a20f1caa78398827155a99da795fe
jomi707 <dol7966@gmail.com>
2022-07-06 16:33:30 +0900

    dom으로 html 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log("hihi");
+const id = document.querySelector("#id"),
+    pswd = document.querySelector("#pswd"),
+    loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        pswd: pswd.value,
+    };
+    console.log(req);
+}
