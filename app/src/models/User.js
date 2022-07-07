2ab509f241d67b784d7733a4e0970ac3ed4ca990
jomi707 <dol7966@gmail.com>
2022-07-07 14:58:27 +0900

    register.ejs .css 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -9,8 +9,9 @@ class User {
 
     login() {
         const body = this.body;
-        console.log(body);
         const { id, pswd } = UserStorage.getUserInfo(body.id);
+        // getUserInfo의 return 값이 id를 이용해 pswd를 가져옴
+        // 굳이 그렇게 하나 싶지만 name이라는 값을 가져올 수도 있기에 그런듯?(유지보수 등 이점)
 
         if (id) {
             if (id === body.id && pswd === body.pswd) {
